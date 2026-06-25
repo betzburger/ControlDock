b3a43f6cbf645bf56ccecaffe257968ab0b08098
Peter Betz <peter.betz@gmail.com>
2026-06-25 09:44:53 +0200

    Initial Commit

---
+++ ControlDock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ControlDock
+//
+//  Created by Peter Betz on 25.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ControlDock/ControlDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlDockApp.swift
+//  ControlDock
+//
+//  Created by Peter Betz on 25.06.26.
+//
+
+import SwiftUI
+
+@main
+struct ControlDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
